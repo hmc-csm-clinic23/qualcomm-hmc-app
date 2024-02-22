4955ff531a31f654d3e028efd781f468a00d430e
tanvikad <tanvika.dasari@gmail.com>
2024-02-21 20:08:16 -0800

    chat bot try

---
+++ bert/src/main/java/com/qualcomm/qti/qa/ui/ChatActivity.java
@@ -0,0 +1,115 @@
+package com.qualcomm.qti.qa.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.qualcomm.qti.R;
+
+
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.JsonObjectRequest;
+//import com.android.volley.toolbox.Volley;
+
+import com.qualcomm.qti.qa.chat.MessageModel;
+import com.qualcomm.qti.qa.chat.MessageRVAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ChatActivity extends AppCompatActivity {
+
+    // creating variables for our
+    // widgets in xml file.
+    private RecyclerView chatsRV;
+    private ImageButton sendMsgIB;
+    private EditText userMsgEdt;
+    private final String USER_KEY = "user";
+    private final String BOT_KEY = "bot";
+
+    // creating a variable for
+    // our volley request queue.
+//    private RequestQueue mRequestQueue;
+
+    // creating a variable for array list and adapter class.
+    private ArrayList<MessageModel> messageModelArrayList;
+    private MessageRVAdapter messageRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        // on below line we are initializing all our views.
+        chatsRV = findViewById(R.id.idRVChats);
+        sendMsgIB = findViewById(R.id.idIBSend);
+        userMsgEdt = findViewById(R.id.idEdtMessage);
+
+        // below line is to initialize our request queue.
+//        mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+//        mRequestQueue.getCache().clear();
+
+        // creating a new array list
+        messageModelArrayList = new ArrayList<>();
+
+        // adding on click listener for send message button.
+        sendMsgIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking if the message entered
+                // by user is empty or not.
+                if (userMsgEdt.getText().toString().isEmpty()) {
+                    // if the edit text is empty display a toast message.
+                    Toast.makeText(ChatActivity.this, "Please enter your message..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // calling a method to send message
+                // to our bot to get response.
+                sendMessage(userMsgEdt.getText().toString());
+
+                // below line we are setting text in our edit text as empty
+                userMsgEdt.setText("");
+            }
+        });
+
+        // on below line we are initializing our adapter class and passing our array list to it.
+        messageRVAdapter = new MessageRVAdapter(messageModelArrayList, this);
+
+        // below line we are creating a variable for our linear layout manager.
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this, RecyclerView.VERTICAL, false);
+
+        // below line is to set layout
+        // manager to our recycler view.
+        chatsRV.setLayoutManager(linearLayoutManager);
+
+        // below line we are setting
+        // adapter to our recycler view.
+        chatsRV.setAdapter(messageRVAdapter);
+    }
+
+    private void sendMessage(String userMsg) {
+        // below line is to pass message to our
+        // array list which is entered by the user.
+        messageModelArrayList.add(new MessageModel(userMsg, USER_KEY));
+        messageRVAdapter.notifyDataSetChanged();
+
+        String botResponse = "figure this out";
+        messageModelArrayList.add(new MessageModel(botResponse, BOT_KEY));
+
+        // notifying our adapter as data changed.
+        messageRVAdapter.notifyDataSetChanged();
+
+    }
+}
