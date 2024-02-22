341852af9471529edf1e7c39a3745aafe15f662e
tanvikad <tanvika.dasari@gmail.com>
2024-02-21 18:40:32 -0800

    added a runtime

--- bert/src/main/java/com/qualcomm/qti/qa/ui/ContextActivity.java
+++ bert/src/main/java/com/qualcomm/qti/qa/ui/ContextActivity.java
@@ -42,10 +42,8 @@ public class ContextActivity extends AppCompatActivity implements AdapterView.On
     String modelUsed = "electra_small_squad2_cached.dlc";
     int modelPos = 0;
     String[] DLCFiles = {  "Electra Small Squad2", "Distilbert", };
-//String[] DLCFiles = { "Electra" };
 
-        String[] DLCPaths = {"electra_small_squad2_cached.dlc", "distilbert_cached.dlc"};
-//        String[] DLCPaths = {"electra_small_squad2_cached.dlc"};
+    String[] DLCPaths = {"electra_small_squad2_cached.dlc", "distilbert_cached.dlc"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +64,6 @@ public class ContextActivity extends AppCompatActivity implements AdapterView.On
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if(contextText.getText().toString().trim().length() > 0 && questionText.getText().toString().trim().length() > 0) {
-//                    Toast.makeText(ContextActivity.this, questionText.getText().toString().trim(), Toast.LENGTH_SHORT).show();
                     questionAnswered = false;
                     answerQuestion(questionText.getText().toString().trim(), contextText.getText().toString().trim());
                 }else {
@@ -98,16 +95,11 @@ public class ContextActivity extends AppCompatActivity implements AdapterView.On
         handler.post(
                 ()-> {
                     String init_files = qaClient.loadModel(modelUsed);
-//                    Toast.makeText(getApplicationContext(), init_files ,Toast.LENGTH_SHORT).show();
                     qaClient.loadDictionary();
 
                 }
         );
-//        String init_files = qaClient.loadModel(modelUsed);
-//        Toast.makeText(getApplicationContext(), init_files ,Toast.LENGTH_SHORT).show();
 
-
-//        Toast.makeText(getApplicationContext(), "Selected DLC: "+modelUsed ,Toast.LENGTH_SHORT).show();
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -117,16 +109,10 @@ public class ContextActivity extends AppCompatActivity implements AdapterView.On
     @Override
     protected void onStart() {
         super.onStart();
-//        Toast.makeText(ContextActivity.this, modelUsed, Toast.LENGTH_SHORT).show();
-//        String initLogs = qaClient.loadModel(modelUsed);
-//        Toast.makeText(ContextActivity.this, initLogs, Toast.LENGTH_SHORT).show();
-//
-//        qaClient.loadDictionary();
 
         handler.post(
                 ()-> {
                     String init_files = qaClient.loadModel(modelUsed);
-//                    Toast.makeText(getApplicationContext(), init_files ,Toast.LENGTH_SHORT).show();
                     qaClient.loadDictionary();
 
                 }
@@ -156,30 +142,12 @@ public class ContextActivity extends AppCompatActivity implements AdapterView.On
         }
         final String questionToAsk = question;
         answerText.setText("Finding the answer with " + DLCFiles[modelPos]);
-//        questionEditText.setText(questionToAsk);
 
-        // Delete all pending tasks.
         handler.removeCallbacksAndMessages(null);
 
-        // Hide keyboard and dismiss focus on text edit.
-//        InputMethodManager imm =
-//                (InputMethodManager) getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
-//        imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
-//        View focusView = getCurrentFocus();
-//        if (focusView != null) {
-//            focusView.clearFocus();
-//        }
-
-        // Reset content text view
-//        contentTextView.setText(content);
 
         questionAnswered = false;
-//
-//        Snackbar runningSnackbar =
-//                Snackbar.make(contentTextView, "Looking up answer...", Snackbar.LENGTH_INDEFINITE);
-//        runningSnackbar.show();
 
-        // Run TF Lite model to get the answer.
         handler.post(
                 () -> {
                     String runtime= "CPU";
@@ -189,39 +157,18 @@ public class ContextActivity extends AppCompatActivity implements AdapterView.On
                     long beforeTime = System.currentTimeMillis();
                     final List<QaAnswer> answers = qaClient.predict(questionToAsk, context, runtime, execStatus);
 
-//                    Toast.makeText(ContextActivity.this, "got here 2", Toast.LENGTH_SHORT).show();
 
                     long afterTime = System.currentTimeMillis();
                     double totalSeconds = (afterTime - beforeTime) / 1000.0;
+                    String displayMessage = runtime + " inference took : ";
+                    displayMessage = String.format("%s %.3f sec. with %s", displayMessage, totalSeconds, DLCFiles[modelPos]);
+                    Toast.makeText(ContextActivity.this, displayMessage, Toast.LENGTH_SHORT).show();
 
                     if (!answers.isEmpty()) {
 
-//                        Toast.makeText(ContextActivity.this, "got here 3", Toast.LENGTH_SHORT).show();
-
-                        // Get the top answer
                         QaAnswer topAnswer = answers.get(0);
-//                        Toast.makeText(ContextActivity.this, topAnswer.text, Toast.LENGTH_SHORT).show();
                         answerText.setText(topAnswer.text);
 
-                        // Show the answer.
-//                        runOnUiThread(
-//                                () -> {
-////                                    runningSnackbar.dismiss();
-////                                    presentAnswer(topAnswer.text);
-//                                    Toast.makeText(ContextActivity.this, topAnswer.text, Toast.LENGTH_SHORT).show();
-//
-//
-////                                    String displayMessage = runtime + " runtime took : ";
-////                                    if (DISPLAY_RUNNING_TIME) {
-////                                        displayMessage = String.format("%s %.3f sec.", displayMessage, totalSeconds);
-////                                    }
-////                                    if (! execStatus.toString().isEmpty())
-////                                        Snackbar.make(contentTextView, execStatus.toString(), Snackbar.LENGTH_LONG).show();
-////                                    else
-////                                        Snackbar.make(contentTextView, displayMessage, Snackbar.LENGTH_LONG).show();
-//
-//                                    questionAnswered = true;
-//                                });
                     }else {
                         Toast.makeText(ContextActivity.this, "Failed to get an answer", Toast.LENGTH_SHORT).show();
 
