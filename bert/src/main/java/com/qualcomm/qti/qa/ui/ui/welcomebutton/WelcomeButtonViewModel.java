a07d9a7865fd00f202ca3f35c3aa4c6d15072730
jjansenmontoya <jjansenmontoya@g.hmc.edu>
2024-03-02 16:52:50 -0800

    updated UI and tested on hw test kit.

--- bert/src/main/java/com/qualcomm/qti/qa/ui/ui/welcomebutton/WelcomeButtonViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.qualcomm.qti.qa.ui.ui.welcomebutton;
-
-import androidx.lifecycle.ViewModel;
-
-public class WelcomeButtonViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
