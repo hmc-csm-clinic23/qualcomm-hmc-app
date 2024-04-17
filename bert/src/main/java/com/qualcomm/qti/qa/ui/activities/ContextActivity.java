da34f18d85d70eeccdb6221ecdeb60d082e9518c
jjansenmontoya <jjansenmontoya@g.hmc.edu>
2024-04-16 17:12:17 -0700

    clean-up, deleting of most Toast (ones I know are not necessary) & unused imports.

--- bert/src/main/java/com/qualcomm/qti/qa/ui/activities/ContextActivity.java
+++ bert/src/main/java/com/qualcomm/qti/qa/ui/activities/ContextActivity.java
@@ -8,7 +8,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
